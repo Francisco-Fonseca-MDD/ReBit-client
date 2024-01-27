520528c735a3b461e6fa0a173314d7e22914b196
Francisco Fonseca <francisco.fonseca2000@gmail.com>
2024-01-27 13:17:05 +0000

    test sentiment analysis package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import Sentiment from "sentiment";
+import axios from "axios";
+
+const sentiment = new Sentiment();
+
+const id = 1172470;
+async function api(gameId) {
+  const { data } = await axios.get(`http://localhost:8080/${gameId}`);
+  const results = data.reviews.map((reviewObj) => {
+    return {
+      review: reviewObj.review,
+      score: sentiment.analyze(reviewObj.review).score,
+    };
+  });
+  // const avgScore =
+  //   results.reduce((sum, currentResult) => {
+  //     return sum + currentResult.score;
+  //   }, 0) / results.length;
+  console.log(results);
+}
+const reviews = api(id);
+const gradientCalculator = (score) => {
+  if (score < 0) return `(255,${255 * score},${255 * score})`;
+};
+
+const reviewCard = () => {};
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {reviews.map((review) => {
+        return <article></article>;
+      })}
+    </div>
+  );
 }
 
 export default App;
