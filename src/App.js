14a4c2a825dc34988e1553828aa87e1c266c69d5
Francisco Fonseca <francisco.fonseca2000@gmail.com>
2024-01-29 00:12:29 +0000

    display reviews for testing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import Sentiment from "sentiment";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const sentiment = new Sentiment();
 
-const id = 1172470;
-async function api(gameId) {
-  const { data } = await axios.get(`http://localhost:8080/${gameId}`);
-  const results = data.reviews.map((reviewObj) => {
-    return {
-      review: reviewObj.review,
-      score: sentiment.analyze(reviewObj.review).score,
-    };
-  });
-  // const avgScore =
-  //   results.reduce((sum, currentResult) => {
-  //     return sum + currentResult.score;
-  //   }, 0) / results.length;
-  console.log(results);
-}
-const reviews = api(id);
-const gradientCalculator = (score) => {
-  if (score < 0) return `(255,${255 * score},${255 * score})`;
-};
-
-const reviewCard = () => {};
-
 function App() {
+  const [reviews, setReviews] = useState(null);
+  const id = 1172470;
+  const gradientCalculator = (score) => {
+    if (score < 0) return `(255,${255 * score},${255 * score})`;
+  };
+  async function api(gameId) {
+    const { data } = await axios.get(`http://localhost:8080/${gameId}`);
+    const results = data.reviews.map((reviewObj) => {
+      return {
+        review: reviewObj.review,
+        score: sentiment.analyze(reviewObj.review).score,
+      };
+    });
+    setReviews(results);
+  }
+
+  useEffect(() => {
+    api(id);
+  }, []);
+
+  const reviewCard = () => {};
+  if (!reviews) return;
   return (
     <div className="App">
-      {reviews.map((review) => {
-        return <article></article>;
+      {reviews.map((review, i) => {
+        return (
+          <article
+            key={i}
+            style={{
+              backgroundColor: `rgb${gradientCalculator(review.score)}`,
+            }}
+          >{`review: ${review.review}`}</article>
+        );
       })}
     </div>
   );
