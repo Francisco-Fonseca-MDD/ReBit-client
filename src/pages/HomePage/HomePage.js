4b196a88fcdcacb54e65b33b8129d399188606c8
Francisco Fonseca <francisco.fonseca2000@gmail.com>
2024-02-04 17:12:49 +0000

    create carousel and large game card component

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,17 +2,42 @@ import "./HomePage.scss";
 import backendApi from "../../utils";
 import LargeGameCard from "./LargeGameCard/LargeGameCard";
 import { useEffect, useState } from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 function HomePage() {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 2,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   const [gamesList, setGamesList] = useState(null);
 
   useEffect(() => {
-    setGamesList(backendApi.fetchAllGames(setGamesList));
+    backendApi.fetchAllGames(setGamesList);
   }, []);
 
   if (!gamesList) return <h1>Loading...</h1>;
   return (
     <main>
-      <LargeGameCard gamesList={gamesList} />
+      <Carousel responsive={responsive} className="carousel">
+        {gamesList.map((game) => {
+          return <LargeGameCard game={game} />;
+        })}
+      </Carousel>
+
       <section className="sorting">
         <div>sorting and filters here</div>
       </section>
--- src/pages/HomePage/LargeGameCard/LargeGameCard.js
+++ src/pages/HomePage/LargeGameCard/LargeGameCard.js
@@ -1,9 +1,28 @@
-function LargeGameCard({ gamesList }) {
+import { useNavigate } from "react-router";
+import "./LargeGameCard.scss";
+function LargeGameCard({ game }) {
+  const navigate = useNavigate();
   return (
-    <article className="LargeGameCard">
+    <article
+      className="LargeGameCard"
+      key={game.id}
+      style={{ backgroundImage: `url(${game.header_url})` }}
+      onClick={() => {
+        navigate(`/games/${game.id}`);
+      }}
+    >
       <div className="LargeGameCard__container">
-        <h1 className="LargeGameCard__title">{gamesList[0].game}</h1>
-        <h2 className="LargeGameCard__score"></h2>
+        <h1 className="LargeGameCard__title">{game.game}</h1>
+        <h2
+          className="LargeGameCard__score"
+          style={{ color: `${game.score <= 0 ? "red" : "green"}` }}
+        >
+          {game.score}
+        </h2>
+      </div>
+      <div className="LargeGameCard__details">
+        <p className="LargeGameCard__text">{game.short_description}</p>
+        <p className="LargeGameCard__text">{String(game.tags)}</p>
       </div>
     </article>
   );
